5128c7c55cd1cff969f598bf377efe75085ee46d
Lucas <asdk51842@gmail.com>
2018-06-30 02:38:53 +0800

    insert

--- insert.php
+++
@@ -1,9 +0,0 @@
-<html>
-<body>
-<?php
-$db = pg_connect("host=140.119.163.105 port=5432 dbname=book_sale user=jack password=jack850912");
-$query = "INSERT INTO author (author_name) VALUES ('$_POST[fname]')";
-$pg_query($query); 
-?>
-</body>
-</html>
